e6f2cc0d250efa8ad031de3f058af6c8d84c0cc8
Marcus Blättermann <kontakt@essenmitsosse.de>
2019-02-25 01:59:58 +0100

    Fix wrong implementation with substraction and devision

--- src/befungeFunctions/index.ts
+++ src/befungeFunctions/index.ts
@@ -16,9 +16,9 @@ const befungeFunctions: { [ key: string ]: BefungeFunction } = {
 	'.': getPopFunction( popped => ( { output: popped.toString() } ) ),
 	'_': getPopFunction( popped => ( { move: { x: popped === 0 ? 1 : -1 } } ) ),
 	'+': getPopTwiceOutputFunction( ( pop1, pop2 ) => pop1 + pop2 ),
-	'-': getPopTwiceOutputFunction( ( pop1, pop2 ) => pop1 - pop2 ),
+	'-': getPopTwiceOutputFunction( ( pop1, pop2 ) => pop2 - pop1 ),
 	'*': getPopTwiceOutputFunction( ( pop1, pop2 ) => pop1 * pop2 ),
-	'/': getPopTwiceOutputFunction( ( pop1, pop2 ) => pop1 / pop2 ),
+	'/': getPopTwiceOutputFunction( ( pop1, pop2 ) => pop2 / pop1 ),
 	':': ( { stack } ) => ( { newStack: [ ...stack, getLastStackValue( stack ) ] } ),
 	'?': () => ( {
 		move: { [ Math.random() > 0.5 ? 'x' : 'y' ]: Math.random() > 0.5 ? -1 : 1 },
